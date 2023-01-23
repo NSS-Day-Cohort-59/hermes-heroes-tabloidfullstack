dee8aabe981f20de6de69fd52dfcf452f9c156f1
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2023-01-23 15:38:46 -0600

    Initial commit

---
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Hello from "./Hello";
+
+export default function ApplicationViews({ isLoggedIn }) {
+  return (
+    <main>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={isLoggedIn ? <Hello /> : <Navigate to="/login" />}
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<p>Whoops, nothing here...</p>} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
---
+++ Tabloid/client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+};
+firebase.initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+reportWebVitals();
