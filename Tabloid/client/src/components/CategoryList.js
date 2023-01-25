71a7d975baff0ca153b189af76f13db239249eaf
Jesier <jesiersantiago@gmail.com>
2023-01-25 14:38:16 -0600

    commit

--- Tabloid/client/src/components/CategoryList.js
+++ Tabloid/client/src/components/CategoryList.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { getAllCategories } from "../modules/categoryManager";
+import { useNavigate } from "react-router-dom";
 
 const CategoryList = () => {
   const [categories, setCategories] = useState([]);
+  const navigate = useNavigate()
 
   const getCategories = () => {
     getAllCategories().then(categories => setCategories(categories));
@@ -13,11 +15,14 @@ const CategoryList = () => {
   }, []);
 
   return (
+    <>
     <div>
       {categories.map(c => 
         <div key={c.id}>{c.name}</div>
       )}
     </div>
+
+    </>
   );
 }
 
