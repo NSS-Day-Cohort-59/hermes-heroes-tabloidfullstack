b989c16df92da239dd773f813b9711c6620acd7b
Ryan Allred <ryanallred91@gmail.com>
2023-01-26 15:39:58 -0600

    stuff

--- Tabloid/client/src/components/Header.js
+++ Tabloid/client/src/components/Header.js
@@ -44,14 +44,14 @@ export default function Header({ isLoggedIn }) {
                 <NavItem>
                   <NavLink tag={RRNavLink} to="/comments">Comments</NavLink>
                 </NavItem>
-
+                  <NavItem>
+                  <NavLink tag={RRNavLink} to="/post">Posts</NavLink>
+                </NavItem>
                 <NavItem>
                   <a aria-current="page" className="nav-link"
                     style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
                 </NavItem>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/post">Posts</NavLink>
-                </NavItem>
+               
               </>
             }
             {!isLoggedIn &&
